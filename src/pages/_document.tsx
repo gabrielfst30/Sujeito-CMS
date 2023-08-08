7825a3d880e3b8890d910f3774e055525d88306d
gabrielfst30 <gabrielfst30@gmail.com>
2023-08-08 14:45:22 -0300

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,900;1,700&display=swap" rel="stylesheet" />
+      <link rel="shortcut icon" href="/favicon.png" type='image/png' />
       <body>
         <Main />
         <NextScript />
@@ -11,3 +14,4 @@ export default function Document() {
     </Html>
   )
 }
+
