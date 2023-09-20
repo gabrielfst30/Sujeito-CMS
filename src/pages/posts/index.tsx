9acb613ca2bff4e4a7882de9f4e9f2534c27e3c7
gabrielfst30 <gabrielfst30@gmail.com>
2023-09-20 14:52:00 -0300

    feature: fazendo paginação

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -29,11 +29,72 @@ type Post = {
 
 interface PostsProps {
   posts: Post[];
+  page: string;
+  totalPage: string;
 }
 
-export default function Posts({ posts: postsBlog }: PostsProps) {
+export default function Posts({
+  posts: postsBlog,
+  page,
+  totalPage,
+}: PostsProps) {
+  //página atual
+  const [currentPage, setCurrentPage] = useState(Number(page));
   //recebe todos os posts ou uma array vazia
   const [posts, setPosts] = useState(postsBlog || []);
+
+  //buscar novos posts de acordo com a página
+  async function reqPost(pageNumber: number) {
+    //INSTANCIANDO O CLIENT
+    const prismic = getPrismicClient();
+
+    //PEGANDO O TIPO DE DOCUMENTO
+    const response = await prismic.query([
+      Prismic.Predicates.at("document.type", "post")
+    ], {
+      orderings: "[document.last_publication_date desc]", //Ordernar posts pelo mais recente
+      fetch: ["post.title", "post.description", "post.cover"], //FILTANDO oque vai vir no response
+      pageSize: 3,
+      page: String(pageNumber)
+    })
+
+    return response;
+  }
+
+  //No paramentro pegamos o numero atual da página
+  async function navigatePage(pageNumber: number) {
+
+    //buscamos todos os dados da página
+    const response = await reqPost(pageNumber);
+
+    if (response.results.length === 0) {
+      return;
+    }
+    //tratando a página
+    const getPosts = response.results.map((post) => {
+      return {
+        slug: post.uid, //nome do post que ficar armazenado no uid do documento
+        title: RichText.asText(post.data.title), //rich text, consome só o text
+        description:
+          post.data.description.find(
+            (content: { type: string }) => content.type === "paragraph"
+          )?.text ?? "", //buscando o paragráfo dentro da descrição
+        cover: post.data.cover.url,
+        updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+          "pt-BR",
+          {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }
+        ), //ultima vez que foi atualizado
+      };
+    });
+    //retornando pagina atual
+    setCurrentPage(pageNumber)
+    //retornando os posts pro use state
+    setPosts(getPosts)
+  }
 
   return (
     <>
@@ -43,7 +104,7 @@ export default function Posts({ posts: postsBlog }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <Link key={post.slug} href={`/posts/${post.slug}`}legacyBehavior>
+            <Link key={post.slug} href={`/posts/${post.slug}`} legacyBehavior>
               <a key={post.slug}>
                 <Image
                   src={post.cover}
@@ -62,23 +123,26 @@ export default function Posts({ posts: postsBlog }: PostsProps) {
           ))}
 
           <div className={styles.buttonNavigate}>
-            <div>
-              <button>
-                <FiChevronsLeft size={25} color="#fff" />
-              </button>
-              <button>
-                <FiChevronLeft size={25} color="#fff" />
-              </button>
-            </div>
-
-            <div>
-              <button>
-                <FiChevronRight size={25} color="#fff" />
-              </button>
-              <button>
-                <FiChevronsRight size={25} color="#fff" />
-              </button>
-            </div>
+            {Number(currentPage) >= 2 && (
+              <div>
+                <button onClick={() => navigatePage(1)}>
+                  <FiChevronsLeft size={25} color="#fff" />
+                </button>
+                <button onClick={() => navigatePage(Number(currentPage - 1))}>
+                  <FiChevronLeft size={25} color="#fff" />
+                </button>
+              </div>
+            )}
+            {Number(currentPage) < Number(totalPage) && (
+              <div>
+                <button onClick={() => navigatePage(Number(currentPage + 1))}>
+                  <FiChevronRight size={25} color="#fff" />
+                </button>
+                <button onClick={() => navigatePage(Number(totalPage))}>
+                  <FiChevronsRight size={25} color="#fff" />
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </main>
@@ -124,6 +188,8 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       posts,
+      page: response.page, //qual página estamos
+      totalPage: response.total_pages, //total de página que temos
     },
     revalidate: 60 * 30, //Atualiza a cada 30 minutos.
   };
